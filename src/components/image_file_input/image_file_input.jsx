260d83d6cffb40ede1761d0681c3b7ae1af29483
Songa0 <dolphin2440@naver.com>
2021-06-24 21:16:46 +0900

    Add input file element

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./image_file_input.module.css";
 
-const ImageFileInput = (props) => (
-  <button className={styles.button}>Image</button>
-);
+const ImageFileInput = () => {
+  const buttonRef = useRef();
+  let inputFile = "";
+  const onFileInputClick = () => {
+    inputFile.click();
+  };
+  return (
+    <button
+      className={styles.button}
+      ref={buttonRef}
+      onClick={onFileInputClick}
+    >
+      <input
+        type="file"
+        accept="image/*"
+        className={styles.file}
+        ref={(input) => (inputFile = input)}
+      />
+    </button>
+  );
+};
 
 export default ImageFileInput;
---
+++ src/service/cloudinary.js
@@ -0,0 +1,3 @@
+class Cloudinary {
+  constructor(key) {}
+}
