97847d4941823928538d46107d59424fb9432678
Songa0 <dolphin2440@naver.com>
2021-06-28 22:00:42 +0900

    Add img upload func tmp

--- src/service/cloudinary.js
+++ src/service/cloudinary.js
@@ -1,3 +1,16 @@
 class Cloudinary {
-  constructor(key) {}
+  upload(formData) {
+    const url = "https://api.cloudinary.com/v1_1/dpu0cdc9t/image/upload";
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {});
+  }
 }
+
+export default Cloudinary;
